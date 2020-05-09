4ee834859b7933f2582a22d87eff1013211b327e
Svensson Jeremy <svensson.jeremy94@gmail.com>
2020-05-09 16:34:03 +0200

    Updated Episode7.py with correct text update

--- Episode7.py
+++ Episode7.py
@@ -3,12 +3,23 @@
 from tkinter import *
 from tkinter.messagebox import *
 
-def updateValues():
+def managePopup():
     if askyesno('Oups !', 'Il semblerait que ce bouton ne serve a rien !'):
         showwarning('Oui', 'Tant pis...')
     else:
         showinfo('Non', 'Et pourtant il semblerait que si...')
         showerror("Private Joke", "Même si c'est dommage :x")
+
+def updateValues():
+    str = input1.get()
+    temp = re.findall(r'\d+', str)
+    res = list(map(int, temp))
+    input2.delete(0, END)
+    input2.insert(0, res[0])
+    input2.insert("end", "CM")
+    input3.delete(0, END)
+    input3.insert(0, res[1])
+    input3.insert("end", "CM")
 
 
 fenetre = Tk()
@@ -17,21 +28,25 @@ Label(fenetre, text="Source :").pack()
 
 value = StringVar()
 value.set("\\B\\T80CM\\T60CM\\N")
-Entry(fenetre, textvariable=value, width=30).pack()
+input1 = Entry(fenetre, textvariable=value, width=30)
+input1.pack()
 
 Button(fenetre, text="Clean", command=updateValues).pack()
 
 Label(fenetre, text="Destination").pack()
 
-value1 = StringVar()
-value1.set("80CM")
-Entry(fenetre, textvariable=value1, width=10).pack(side=LEFT, padx=15)
+value = StringVar()
+value.set("80CM")
+input2 = Entry(fenetre, textvariable=value, width=10)
+input2.pack(side=LEFT, padx=15)
 
-value2 = StringVar()
-value2.set("60CM")
-Entry(fenetre, textvariable=value2, width=10).pack(side=RIGHT, padx=15)
+value = StringVar()
+value.set("60CM")
+input3 = Entry(fenetre, textvariable=value, width=10)
+input3.pack(side=RIGHT, padx=15)
 
 Canvas(fenetre, width=250, height=100, bg='ivory').pack(side=TOP, padx=5, pady=5)
-Button(fenetre, text ='Fermer', command=fenetre.quit).pack(side=LEFT, padx=125, pady=5)
+Button(fenetre, text ='Info', command=managePopup).pack(side=LEFT, padx=100, pady=5)
+Button(fenetre, text ='Fermer', command=fenetre.quit).pack(side=LEFT, padx=150, pady=5)
 
 fenetre.mainloop()
